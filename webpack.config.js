8f1e0cf4942b159801a11a585c2b36d5e8b6d108
marcushugo <phantomboss300@yahoo.com>
2023-02-14 08:09:49 -0600

    updated build command

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "src"),
+    path: path.resolve(__dirname, "./dist"),
     publicPath: "/",
     filename: "bundle.js"
   },
